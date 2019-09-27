ace9265dada78c0a6e76940602feabc1518ca01e
weaming <garden.yuen@gmail.com>
2019-09-28 04:04:16 +0800

    2019-09-28 04:04:16

--- src/realworldhaskell/ch3.hs
+++ src/realworldhaskell/ch3.hs
@@ -12,3 +12,35 @@ fromList (Cons x xs ) = [x] ++ fromList xs
 --              | Nothing
 
 data Tree a = Node a (Maybe a) (Maybe a) deriving (Show)
+
+
+-- 1
+myLength :: [a] -> Int
+myLength ([]    ) = 0
+myLength ([ x ] ) = 1
+myLength (x : xs) = 1 + myLength xs
+
+-- 2
+-- 3
+mean :: [Float] -> Float
+mean xs = sum xs / fromIntegral (myLength xs)
+  where
+    sum :: [Float] -> Float
+    sum []       = 0
+    sum [x     ] = x
+    sum (x : xs) = x + sum xs
+
+-- 4
+ireverse :: [a] -> [a]
+ireverse []  = []
+ireverse [x] = [x]
+ireverse xs  = [last xs] ++ ireverse (init xs)
+
+palindrome :: [a] -> [a]
+palindrome xs = xs ++ ireverse xs
+
+
+-- 5
+-- TODO
+-- isPalindrome :: [a] -> Bool
+-- isPalindrome xs = xs == ireverse xs
