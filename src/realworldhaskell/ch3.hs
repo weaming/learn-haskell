b09ba7c58bb06d70213a4be181aa7d822b28076e
weaming <garden.yuen@gmail.com>
2019-09-29 23:52:31 +0800

    update ch3

--- src/realworldhaskell/ch3.hs
+++ src/realworldhaskell/ch3.hs
@@ -1,3 +1,6 @@
+module Ch3 where
+import           Data.List
+
 data List a = Cons a (List a)
             | Nil
               deriving (Show)
@@ -15,15 +18,15 @@ data Tree a = Node a (Maybe a) (Maybe a) deriving (Show)
 
 
 -- 1
-myLength :: [a] -> Int
-myLength ([]    ) = 0
-myLength ([ x ] ) = 1
-myLength (x : xs) = 1 + myLength xs
+len :: [a] -> Int
+len ([]    ) = 0
+len ([ x ] ) = 1
+len (x : xs) = 1 + len xs
 
 -- 2
 -- 3
 mean :: [Float] -> Float
-mean xs = sum xs / fromIntegral (myLength xs)
+mean xs = sum xs / fromIntegral (len xs)
   where
     sum :: [Float] -> Float
     sum []       = 0
@@ -42,5 +45,19 @@ palindrome xs = xs ++ ireverse xs
 
 -- 5
 -- TODO
--- isPalindrome :: [a] -> Bool
--- isPalindrome xs = xs == ireverse xs
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome xs = xs == ireverse xs
+
+-- 6
+sortByLength :: [[a]] -> [[a]]
+sortByLength xss = sortBy byLength xss
+  where
+    byLength :: [a] -> [a] -> Ordering
+    byLength xs ys = compare (len xs) (len ys)
+
+-- 7
+myintersperse :: a -> [[a]] -> [a]
+myintersperse s []           = []
+myintersperse s [x]          = x
+myintersperse s [x, y]       = x ++ [s] ++ y
+myintersperse s (x : y : xs) = x ++ [s] ++ y ++ (myintersperse s xs)
